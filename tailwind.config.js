2e303f88bf1efe0966c008bdc9bdcb4cf876422d
unknown <sriasrinapitupulu@gmail.com>
2022-10-11 10:53:19 +0700

    upload file

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#14b8a6",
+        secondary: "#64748b",
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1500px",
+      },
+    },
+  },
+  plugins: [],
+};
